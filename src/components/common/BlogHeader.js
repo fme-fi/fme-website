8acb25a861fab64f5346b5b1a57ec644d7336661
Richard Zilahi <zilahi@gmail.com>
2019-01-21 16:40:59 +0200

    implementing blog page

--- src/components/common/BlogHeader.js
+++ src/components/common/BlogHeader.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import { Parallax, Background } from 'react-parallax';
 
 const BlogHeaderWrapper = styled.div``;
-
 const BlogFeaturedImage = styled.img``;
-
 const BlogMeta = styled.div``;
 
 class BlogHeader extends Component {
     render() {
         return(
-            <BlogHeaderWrapper>
-                <BlogFeaturedImage>
-
-                </BlogFeaturedImage>
-                <BlogMeta>
-                    {this.props.blogTitle}
-                </BlogMeta>
-            </BlogHeaderWrapper>
+            <Parallax
+            blur={0}
+            bgImage={this.props.featuredImage}            
+            strength={600} 
+            bgClassName="parallaxBlogHeader"     
+            contentClassName="parallaxContentBlogHeader"                  
+            >            
+            <BlogHeaderWrapper className="blogHeaderWrapper">                
+                    <BlogMeta className="blogMeta">
+                        <h1>
+                            {this.props.blogTitle}
+                        </h1>
+                    </BlogMeta>
+                </BlogHeaderWrapper>
+            </Parallax>      
         )
     }
 }
