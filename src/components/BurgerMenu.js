af9fef34dc89624a6d9a74554703169e5a0771b7
Richard Zilahi <zilahi@gmail.com>
2019-02-23 01:04:25 +0200

    implementing burger menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { FaTimes } from 'react-icons/fa';
 
 class BurgerMenu extends Component {    
     render() {         
         return ( 
             <div className={`burgerMenu`}>
+                <div className="closeContainer">
+                    <FaTimes size={32} color="#1C3142" />
+                </div>
                 <ul>
                     <li className="active">
                         <a href="#">
