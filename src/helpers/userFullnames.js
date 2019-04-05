204ea2796379ee5035f5286a5b96420cdd35cfdd
Richard Zilahi <zilahi@gmail.com>
2019-04-06 00:20:54 +0300

    layout updates + fullNameApi updates

--- src/helpers/userFullnames.js
+++ src/helpers/userFullnames.js
@@ -1,3 +1,6 @@
 export const users = [
     { author: 'zilahir', fullName: 'Richard Zilahi' }
+    { author: 'jelarp', fullName: 'Jelinkó Árpád' },     
+    { author: 'viktooriatooth', fullName: 'Tóth Viktória' },     
+    { author: 'ferencvilisics', fullName: 'Vilisics Ferenc' }
 ];
