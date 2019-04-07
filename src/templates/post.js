cd3cd80bf2cb0215eaf31ce06bdf4c472111719d
Richard Zilahi <zilahi@gmail.com>
2019-04-08 00:25:22 +0300

    issue temp fix

--- src/templates/post.js
+++ src/templates/post.js
@@ -23,7 +23,7 @@ const PostTemplate = (props) => {
           title={post.title}
           meta={[
             { name: 'description', content: post.title },
-            { name: 'og-image', content: post.featured_media.source_url }
+//            { name: 'og-image', content: post.featured_media.source_url } //TODO fix this
           ]}
         />      
        <article>
