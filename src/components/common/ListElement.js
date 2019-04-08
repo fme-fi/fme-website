b2ad457d337401e80f97b99824ccd14b974f2ba6
Richard Zilahi <zilahi@gmail.com>
2019-04-08 23:45:22 +0300

    hooks added to topMenuBar

--- src/components/common/ListElement.js
+++ src/components/common/ListElement.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-    const ListElement = (props) => {
-        return ( 
-            <li>
-                <a href={props.url}>
-                    <p dangerouslySetInnerHTML={{__html: props.title}} ></p>
-                    <p dangerouslySetInnerHTML={{__html: props.description}}></p>
-                </a>
-            </li>
-         );
-    }
+const ListElement = (props) => {
+    return ( 
+        <li>
+            <a target="_blank" href={props.url}>
+                <p dangerouslySetInnerHTML={{__html: props.title}} ></p>
+                <p dangerouslySetInnerHTML={{__html: props.description}}></p>
+            </a>
+        </li>
+        );
+}
   
-    
-    export default ListElement;
+export default ListElement;
--- src/components/common/TopMenuBar.js
+++ src/components/common/TopMenuBar.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import React, { useState, useEffect } from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 import striptags from 'striptags';
 import styled from 'styled-components';
 import FmeLogo from './../assets/others/logo.svg';
@@ -21,99 +21,61 @@ const FmeLogoImage = styled.img`
 let lastScrollY = 0;
 let ticking = false;
 
-class TopMenuBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuBarFixed: false,            
-            currentPage: null
+
+export default (props) => {    
+
+    const [currentPage, setCurrentPage] = useState("");
+
+    useEffect( () => {
+        let urlArray = window.location.href.split("/"); 
+        console.log("urlArray", urlArray)       
+        setCurrentPage(urlArray[urlArray.length - 1])
+    })
+
+    const data = useStaticQuery(graphql`
+        query MenuStructureQuery {
+            allWordpressPage(filter: { wordpress_parent: {eq: 532} }) {
+                edges {
+                node {
+                    id
+                    title
+                    content
+                    excerpt
+                    date
+                    modified
+                    slug
+                    status, 
+                    menu_order
+                }
+                }
+            }    
         }
-        this.handleScroll = this.handleScroll.bind(this);
-    }
+    `);
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-        let urlArray = window.location.href.split("/");
-        let currentPage = urlArray[urlArray.length - 1];        
-        this.setState({
-            currentPage: currentPage
-        })
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
-
-    handleScroll = () => {
-        lastScrollY = window.scrollY;
-        if (!ticking) {
-        window.requestAnimationFrame(() => {
-            if (lastScrollY > 180) {
-                this.setState({
-                    isMenuBarFixed: true
-                })
-            } else {
-                this.setState({
-                    isMenuBarFixed: false
-                })
-            }
-            ticking = false;
-            });
-            ticking = true;
-        }
-    }
-    render() {
-        let fixMenuBar = this.state.isMenuBarFixed ? 'fixed' : '';
-        let isSubPage = this.props.subPage ? 'subPage' : '';        
-        return (
-            <StaticQuery
-                query={graphql`
-                    query MenuStructureQuery {
-                        allWordpressPage(filter: { wordpress_parent: {eq: 532} }) {
-                            edges {
-                            node {
-                                id
-                                title
-                                content
-                                excerpt
-                                date
-                                modified
-                                slug
-                                status        
-                            }
-                            }
-                        }    
+    const result = data.allWordpressPage.edges.sort( (a,b) => {  
+        return b.node.menu_order - a.node.menu_order
+    });
+    let fixMenuBar = ''
+    let isSubPage = props.subPage ? 'subPage' : '';        
+    console.log("currentPage", currentPage)
+    return (                        
+            <div className={`topMenuContainer ${isSubPage}`}>                    
+                <div className={`topMenuBar ${fixMenuBar}`}>
+                    {
+                        props.isMenuBarFixed ? 
+                        <MemberShipFee />
+                        : null
                     }
-                `}
-            
-                render={data => (                
-                <div className={`topMenuContainer ${isSubPage}`}>                    
-                    <div className={`topMenuBar ${fixMenuBar}`}>
-                        {
-                            this.state.isMenuBarFixed ? 
-                            <MemberShipFee />
-                            : null
-                        }
-                        <ul>
-                        <li>
-                            <Link to="/">
-                                <FmeLogoImage src={FmeLogo} />
-                            </Link>
-                        </li>
-                        {
-                            data.allWordpressPage.edges.map(({node}) => (
-                                    this.state.currentPage === node.slug ?
-                                        <li className="activePage" key={node.slug}>
-                                            <Link to={`/${node.slug}`}>
-                                                {node.title}
-                                                <p>
-                                                    {
-                                                        striptags(node.excerpt)
-                                                    }
-                                                </p>
-                                            </Link>
-                                        </li>
-                                    : <li key={node.slug}>
+                    <ul>
+                    <li>
+                        <Link to="/">
+                            <FmeLogoImage src={FmeLogo} />
+                        </Link>
+                    </li>
+                    {
+                        result.map(({node}) => (
+                                currentPage === node.slug ?
+                                    <li className="activePage" key={node.slug}>
                                         <Link to={`/${node.slug}`}>
                                             {node.title}
                                             <p>
@@ -122,29 +84,33 @@ class TopMenuBar extends Component {
                                                 }
                                             </p>
                                         </Link>
-                                    </li>                                
-                            ))
-                        }
-                        </ul>
-                        <ul className={isSubPage}>
-                            <li>
-                                Közösségi média
-                            </li>
-                            <li>
-                                <FaInstagram size="1.1em" />
-                            </li>
-                            <li>
-                                <FaFacebookF size="1.1em" />
-                            </li>
-                        </ul>
-                    </div>          
-                </div>
-                    
-                
-            )}
-            />
-        )
-    }
+                                    </li>
+                                : <li key={node.slug}>
+                                    <Link to={`/${node.slug}`}>
+                                        {node.title}
+                                        <p>
+                                            {
+                                                striptags(node.excerpt)
+                                            }
+                                        </p>
+                                    </Link>
+                                </li>                                
+                        ))
+                    }
+                    </ul>
+                    <ul className={isSubPage}>
+                        <li>
+                            Közösségi média
+                        </li>
+                        <li>
+                            <FaInstagram size="1.1em" />
+                        </li>
+                        <li>
+                            <FaFacebookF size="1.1em" />
+                        </li>
+                    </ul>
+                </div>          
+            </div>             
+    )
+    
 }
-
-export default TopMenuBar;
