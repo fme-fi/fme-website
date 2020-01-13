44bb450ec241c432c33caa6d5b8fa9d9541114f1
Richard Zilahi <richard.zilahi@salesframe.com>
2020-01-13 10:17:27 +0200

    implementing calendar component

--- src/components/EventsCalendar/EventsCalendar.js
+++ src/components/EventsCalendar/EventsCalendar.js
@@ -9,9 +9,15 @@ import styles from './EventsCalendar.module.scss'
 
 const EventsCalendar = () => {
     const [days, setDays] = useState([])
+    const [currentDate, setCUrrentDate] = useState(new Date())
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth() + 1)
     useEffect(() => {
         console.debug('rendering', true)
-    }, [])
+        const d = currentDate
+        const currentYear = d.getFullYear()
+        const getDaysInMonth = (month, year) => (new Array(31)).fill('').map((v,i ) => new Date(year,month - 1, i +1 )).filter(v => v.getMonth ()=== month - 1)
+        console.debug(getDaysInMonth(currentMonth, currentYear))
+    }, [currentDate])
 
     function hasEventThisDay(date) {
         return false
